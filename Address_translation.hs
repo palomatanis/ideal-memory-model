9d3ed6e565c5f2afaa631a2813801e6db563faae
palomatanis <paloma.pedregal@gmail.com>
2018-03-27 12:29:21 +0200

    Reduction

--- Address_translation.hs
+++ Address_translation.hs
@@ -1,21 +1,23 @@
-module Address_translation where
+module Address_translation  where
 
 import System.Random
 import Data.List
+import Data.List.Split
 
------ Address parameters
+-------------- PARAMETERS
+----- Address
 
-virtualAddressLength :: Int
-virtualAddressLength = 48
+virtual_address_length :: Int
+virtual_address_length = 48
 
-physicalAddressLength :: Int
-physicalAddressLength = 34
+physical_address_length :: Int
+physical_address_length = 34
 
--- Bits of offset, between 0 and addressLength
+-- Bits of page index , between 0 and addressLength
 pageOffset :: Int
 pageOffset = 12
 
------ Cache parameters
+----- Cache
 
 -- Number of offset bits (n bits for blocks of 2^n)
 cacheOffset :: Int
@@ -25,69 +27,160 @@ cacheOffset = 6
 cacheSet :: Int
 cacheSet = 13
 
--- Associativity (number of blocks per cache set)
+-- associativity (number of blocks per cache set)
 associativity :: Int 
 associativity = 12
 
 
 ------ TLB
 
--- bits
+-- Has to be multiple of tlb associativity
 tlb_size :: Int
 tlb_size = 1536
--- tlb_size = 10
 
+-- TLB associativity
+tlb_bits :: Int
+tlb_bits = 4
 
 --------------------------------------------------------------------------------------------------------------
 data Address = Address [Int]
+  deriving (Read, Show, Eq)
 
 showAddress :: Address -> [Int]
 showAddress (Address a) = a
 
 -- List of addresses as strings in lines
-toStringAddresses :: [Address] -> String
-toStringAddresses = unlines . map show . map showAddress
+to_string_addresses :: [Address] -> String
+to_string_addresses = unlines . map show . map showAddress
   
 -- Only to be used with valid [Int] 
-createAddress :: [Int] -> Address
-createAddress = Address
+create_address :: [Int] -> Address
+create_address = Address
 
  -- With a seed
 createRandom_Address :: Int -> Int -> Address
 createRandom_Address seed numberBits = Address (take numberBits $ randomRs (0, 1) (mkStdGen seed))
 
 
------
+----- Address translation
 
 -- From a virtual address, create a physical address with same offset and the rest random bits
 virtual_to_physical_translation :: (Address, Int) -> Address
 virtual_to_physical_translation (virtual, seed) = Address ((take (numberOfRandomBits) $ randomRs (0, 1) (mkStdGen seed)) ++ (drop (oldPageBits) $ showAddress virtual))
-  where numberOfRandomBits = physicalAddressLength - pageOffset
-        oldPageBits = virtualAddressLength - pageOffset
+  where numberOfRandomBits = physical_address_length - pageOffset
+        oldPageBits = virtual_address_length - pageOffset
+
+
+---- Binary test
+
+-- Returns 1 if victim address is in eviction set, otherwise 0
+is_address_in_eviction_set :: Address -> [Address] -> Int
+is_address_in_eviction_set victim set =
+  if (length $ filter (== victims_set) $ get_sets_from_addresses set) >= associativity
+  then 1
+  else 0
+  where victims_set = get_set_from_single_address victim
         
+---- Polynomial test
+
+exists_eviction :: [Address] -> Int
+exists_eviction add =
+  if (number_of_eviction_sets add) > 0
+  then 1
+  else 0
 
 -- Returns number of caches with associativity number of addresses
 number_of_eviction_sets :: [Address] -> Int
-number_of_eviction_sets = length . filter (> associativity) . separateAdressesIntoBins
+number_of_eviction_sets = length . filter (> associativity) . separate_addresses_into_bins
 
 
 -- Takes list of addresses and outputs the histogram of sets(as a list) -> [3, 4, 4, 1] means there were 3 "00", 4 "01", 4 "10", 4 "11"
-separateAdressesIntoBins :: [Address] -> [Int]
-separateAdressesIntoBins sets = map (\x -> (length $ filter (==x) $ getSetsFromAddresses sets)) bins
-
--- separateAdressesIntoBins :: [Address] -> [Int]
--- separateAdressesIntoBins sets = map length $ group $ sort $ getSetsFromAddresses sets
+separate_addresses_into_bins :: [Address] -> [Int]
+separate_addresses_into_bins sets = map (\x -> (length $ filter (==x) $ get_sets_from_addresses sets)) $ bins cacheSet
+-- separate_addresses_into_bins :: [Address] -> [Int]
+-- separate_addresses_into_bins sets = map length $ group $ sort $ get_sets_from_addresses sets
 
 
 -- Takes list of addresses and outputs lists of the set part of the addresses (each part of the address is a list)
-getSetsFromAddresses :: [Address] -> [[Int]]
-getSetsFromAddresses = map (take cacheSet) . map (drop (physicalAddressLength - cacheOffset - cacheSet)) . map showAddress
+get_sets_from_addresses :: [Address] -> [[Int]]
+get_sets_from_addresses = map (take cacheSet) . map (drop (physical_address_length - cacheOffset - cacheSet)) . map showAddress
+
+-- Takes list of addresses and outputs lists of the set part of the addresses (each part of the address is a list)
+get_set_from_single_address :: Address -> [Int]
+get_set_from_single_address = take cacheSet . drop (physical_address_length - cacheOffset - cacheSet) . showAddress
+
+
+reduction :: Address -> [Address] -> Int
+reduction victim initial_set =
+  if ((number_addreses == associativity) && (is_address_in_eviction_set victim initial_set) == 1)
+  then 1
+  else
+    case (take_one victim combinations) of
+      Just new_set -> reduction victim new_set
+      Nothing -> 0 
+  where
+    number_addreses = length initial_set
+    cei = ceiling $ (fromIntegral number_addreses) / (fromIntegral $ associativity + 1)
+    trun = truncate $ (fromIntegral number_addreses) / (fromIntegral $ associativity + 1)
+    n_cei = mod number_addreses (associativity + 1)
+    n_trun = (associativity + 1) - n_cei
+    aux = (take n_cei $ repeat cei) ++ (take n_trun $ repeat trun)
+    groups = splitPlaces aux initial_set
+    combinations = map (\x -> initial_set \\ x) groups
+
+
+reduction_alt :: Int -> [Int] -> Int
+reduction_alt v sets =
+  if (number_addreses == associativity) && (is_eviction sets)
+  then 1
+  else
+    case (find is_eviction combinations) of
+      Just new_set -> reduction_alt v new_set
+      Nothing -> 0
+  where
+    number_addreses = length sets
+    cei = ceiling $ (fromIntegral number_addreses) / (fromIntegral $ associativity + 1)
+    trun = truncate $ (fromIntegral number_addreses) / (fromIntegral $ associativity + 1)
+    n_cei = mod number_addreses (associativity + 1)
+    n_trun = (associativity + 1) - n_cei
+    aux = (take n_cei $ repeat cei) ++ (take n_trun $ repeat trun)
+    groups = splitPlaces aux sets
+    combinations = map concat $ map (\x -> deleteN x groups) [0..((length groups) - 1)]
+    is_eviction subs = if ((length $ filter (== v) subs) >= associativity) then True else False
+        
+
+deleteN :: Int -> [a] -> [a]
+deleteN _ []     = []
+deleteN i (a:as)
+   | i == 0    = as
+   | otherwise = a : deleteN (i-1) as
+   
+-- delete_nth :: Int -> [a] -> [a]
+-- delete_nth n [] = []
+-- delete_nth n lst =
+--   let (ys,zs) = splitAt n lst in
+--     ys ++ (tail zs)
+
+
+take_one :: Address -> [[Address]] -> Maybe [Address]
+take_one vic = find (\x -> (is_address_in_eviction_set vic x) == 1)
 
 
 -- Create set combinations
-bins :: [[Int]]
-bins = take (2^cacheSet) $ filter((== cacheSet) . length) $ concat $ iterate ((:) <$> [0, 1] <*>) [[]]
+bins :: Int -> [[Int]]
+bins n_bins = take (2^n_bins) $ filter((== n_bins) . length) $ concat $ iterate ((:) <$> [0, 1] <*>) [[]]
 
 
-number_of_eviction_addresses :: [Int] -> Int
-number_of_eviction_addresses = sum . filter (> associativity)
+number_of_eviction_addresses :: [Address] -> Int
+number_of_eviction_addresses = sum . filter (> associativity) . separate_addresses_into_bins
+
+---- TLB
+tlb_misses :: [Address] -> Int
+tlb_misses = sum . map (\x -> x - tlb_block_size) . filter (> tlb_block_size) . tlb_misses' . map (take tlb_bits) . map (drop $ virtual_address_length - pageOffset - tlb_bits) . map showAddress
+  
+tlb_misses' :: [[Int]] -> [Int]
+tlb_misses' pages = map (\x -> length $ filter (==x) pages) $ bins tlb_bits
+
+
+tlb_block_size :: Int
+tlb_block_size = truncate $ (fromIntegral tlb_size) / (fromIntegral $ 2^tlb_bits)
--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import System.Random
+import System.Random.Shuffle
 import Address_translation
 import Control.Monad
 import System.IO
@@ -23,95 +24,143 @@ iterations = 100
 memoryRange :: Int
 memoryRange = 24
 
+possible_addresses :: Int
+possible_addresses = 2 ^ (virtual_address_length - pageOffset - (virtual_address_length - memoryRange))
 
-printing = do
-  p <- list_semi_random_addresses 100 memoryRange
-  putStrLn $ toStringAddresses p
-  
+
+-- printing = do
+--   p <- list_semi_random_addresses 4000 memoryRange
+--   putStrLn $ show $ exists_eviction p
+--   --putStrLn $ to_string_addresses p
+
+-- printing2 = do
+--   p <- test_reduction_alternative 4000
+--   putStrLn $ show p
+
+
 -- Save tests
 main = do
-    file <- openFile "resultsBlaBla.txt" WriteMode
     m <- test_complete
-    hPrint file $ list_to_string m
-    hClose file
+    appendFile "resultsReduction_alt.txt" ((list_to_string m) ++ "\n")
+    -- writeFile "resultsTest.txt" ((list_to_string m) ++ "\n")
     where list_to_string = unwords . map show
-    
+
+          
 test_complete = do
-  p <- mapM test_n_addresses [numberAddrToTest_From, (numberAddrToTest_From + (2*associativity))..numberAddrToTest_To]
+  p <- mapM (do_test_of test_reduction_alternative) [numberAddrToTest_From, (numberAddrToTest_From + (2*associativity))..numberAddrToTest_To]
   return p
+
   
--- Mean of eviction sets for n addresses
-test_n_addresses :: Int -> IO(Float)  
-test_n_addresses n = do
-  p <-  replicateM iterations $ count_evictions n
+do_test_of :: (Int -> IO(Int)) -> Int -> IO(Float)
+do_test_of f n = do
+  p <- replicateM iterations $ f n
   return (mean p)
 
-         
+
+count_tlb_misses :: Int -> IO(Int)
+count_tlb_misses number = do
+  r <- list_semi_random_addresses number memoryRange
+  return(tlb_misses r)
+
+    
 -- Creates list of n physical addresses from a virtual address, and counts eviction sets
 -- The list is created by choosing n addresses with the same offset and belonging to the same range but each of a different page 
 count_evictions :: Int -> IO(Int)
 count_evictions number = do
-  listR <- generateSeeds number
+  listR <- generate_seeds number
   r <- list_semi_random_addresses number memoryRange
   return (number_of_eviction_sets $ map virtual_to_physical_translation $ zip r listR)
+
+
+-- Creates list of n physical addresses from a virtual address, and counts eviction sets
+-- The list is created by choosing n addresses with the same offset and belonging to the same range but each of a different page 
+count_addresses_evictions :: Int -> IO(Int)
+count_addresses_evictions number = do
+  listR <- generate_seeds number
+  r <- list_semi_random_addresses number memoryRange
+  return (number_of_eviction_addresses $ map virtual_to_physical_translation $ zip r listR)
+
+multinomial_test :: Int -> IO(Int)
+multinomial_test number = do
+  listR <- generate_seeds number
+  r <- list_semi_random_addresses number memoryRange
+  return (exists_eviction $ map virtual_to_physical_translation $ zip r listR)  
+
+
+-- Creates list of addresses, and a random victim address, checks if it's in an eviction set
+binaryTest :: Int -> IO(Int)
+binaryTest number = do
+  listR <- generate_seeds number
+  r <- list_semi_random_addresses number memoryRange
+  let offset = take pageOffset [0,0..0]
+  a <- random_address $ physical_address_length - pageOffset
+  let victim = create_address $ (showAddress a) ++ offset
+  return (is_address_in_eviction_set victim $ map virtual_to_physical_translation $ zip r listR)
+
+test_reduction :: Int -> IO (Int)
+test_reduction number = do
+  listR <- generate_seeds number
+  r <- list_semi_random_addresses number memoryRange
+  let offset = take pageOffset [0,0..0]
+  a <- random_address $ physical_address_length - pageOffset
+  let victim = create_address $ (showAddress a) ++ offset
+  return (reduction victim $ map virtual_to_physical_translation $ zip r listR)  
+
+test_reduction_alternative :: Int -> IO (Int)
+test_reduction_alternative number = do
+  let free_cache_bits = cacheOffset + cacheSet - pageOffset
+  let free_cache = 2 ^ free_cache_bits
+  r <- replicateM number $ randomRIO(0, free_cache - 1)
+  v <- randomRIO(0, free_cache - 1)
+  return (reduction_alt v r)
 
 -- create list of n [0...0] addresses of length nbits (virtual)
 list_of_addresses_same_offset :: Int -> Int -> [Address]
 list_of_addresses_same_offset _ 0 = []
-list_of_addresses_same_offset nbits n = (createAddress $ take nbits [0,0..0]):list_of_addresses_same_offset nbits (n-1)
+list_of_addresses_same_offset nbits n = (create_address $ take nbits [0,0..0]):list_of_addresses_same_offset nbits (n-1)
 
-
--- Simplificacion??
+  
 -- Generates list of n addesses with the same offset [0...0] in a range of 2^range addresses, each from a different page
 -- Range a in bits
 list_semi_random_addresses :: Int -> Int -> IO ([Address])
 list_semi_random_addresses n range = do
   s <- randomIO :: IO(Int)
-  let start = take (virtualAddressLength - pageOffset) $ randomRs (0, 1) (mkStdGen s)
+  let start = take (virtual_address_length - pageOffset) $ randomRs (0, 1) (mkStdGen s)
   let fixed_bits = take number_fixed_bits start
   p <- n_different_pages number_variable_bits n
-  return (map createAddress $ map (\x -> fixed_bits ++ x ++ offset) p)
+  return (map create_address $ map (\x -> fixed_bits ++ x ++ offset) p)
   where
-    number_fixed_bits = virtualAddressLength - range
+    number_fixed_bits = virtual_address_length - range
     number_variable_bits = range - pageOffset
     offset = take pageOffset [0,0..0]
 
--- Takes a number of bits, and the length  and returns a list of random, not repeated [Int]
+
 n_different_pages :: Int -> Int -> IO ([[Int]])
-n_different_pages n_bits len = n_different_pages' n_bits len []
-  -- if ((2^n_bits) < len)
-  --   then do
-  --      p <- list_of_addresses_same_offset n_bits len
-  --      return (map showAddress p)
-  --   else do
-  --      (return (n_different_pages' n_bits len []))
+n_different_pages n_bits len = do
+  p <- shuffleM $ bins n_bits
+  return (take len p)
+   
+-- -- Takes a number of bits, and the length  and returns a list of random, not repeated [Int]
+-- n_different_pages2 :: Int -> Int -> IO ([[Int]])
+-- n_different_pages2 n_bits len = n_different_pages2' n_bits len []
+--   -- if ((2^n_bits) < len)
+--   --   then do
+--   --      p <- list_of_addresses_same_offset n_bits len
+--   --      return (map showAddress p)
+--   --   else do
+--   --      (return (n_different_pages' n_bits len []))
   
-n_different_pages' :: Int -> Int -> [[Int]] -> IO ([[Int]])
-n_different_pages' n_bits len original_list = do
-  let actual_list = nub original_list
-  let length_difference = len - (length actual_list)
-  if (length_difference == 0)
-    then return actual_list
-    else do
-       l <- list_of_random_addresses n_bits length_difference
-       let new_list = nub $ map showAddress l
-       r <- n_different_pages' n_bits len (actual_list ++ new_list)
-       return r
-
-  
-  
-
---   -- Takes a number of bits, and the length  and returns a list of random, not repeated [Int]
--- n_different_pages :: Int -> Int -> IO ([[Int]])
--- n_different_pages n_bits len = do
---   l <- list_of_random_addresses n_bits len
---   let actual_list = nub $ map showAddress l
---   let length_l = length actual_list
---   if (length_l == len)
---     then return (map showAddress l)
+-- n_different_pages2' :: Int -> Int -> [[Int]] -> IO ([[Int]])
+-- n_different_pages2' n_bits len original_list = do
+--   let actual_list = nub original_list
+--   let length_difference = len - (length actual_list)
+--   if (length_difference == 0)
+--     then return actual_list
 --     else do
---        r <- n_different_pages n_bits (len - length_l)
---        return (actual_list ++ r)
+--        l <- list_of_random_addresses n_bits length_difference
+--        let new_list = nub $ map showAddress l
+--        r <- n_different_pages2' n_bits len (actual_list ++ new_list)
+--        return r
 
 
 -- create list of n random addresses of length len (virtual)
@@ -131,11 +180,11 @@ random_address nbits = do
 
 
 -- Generates a list of n seeds
-generateSeeds :: Int -> IO ([Int])
-generateSeeds 0 = return []
-generateSeeds n = do
+generate_seeds :: Int -> IO ([Int])
+generate_seeds 0 = return []
+generate_seeds n = do
   r <- randomIO
-  rs <- generateSeeds (n-1)
+  rs <- generate_seeds (n-1)
   return (r:rs)
 
 
