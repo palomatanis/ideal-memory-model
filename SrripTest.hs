316650c95ae0e14746c06d0e5584eb46e65897fb
palomatanis <paloma.pedregal@gmail.com>
2018-10-18 10:03:26 +0200

    Tests, ploteude

--- SrripTest.hs
+++ SrripTest.hs
@@ -5,15 +5,19 @@ import Data.List
 import Data.Char
 import Control.Monad
 
+import Numeric
+import Data.Maybe
+
 prints = True
+printsAll = False
 save = False
 
 take_pattern = 20
-
 length_trace = 10
 
+bip_probability = 2 -- over 64
 
-path = "./adaptive/srrip_initial/srrip_hp_tests_initial_states_traces_"
+path = "./adaptive/srrip_initial/plru_tests_initial_states_traces_"
 
 printToFileLn :: String -> String -> IO()
 printToFileLn path_end text = do
@@ -22,12 +26,14 @@ printToFileLn path_end text = do
 
 patterns = map (\(c, d, l, n, r) -> ([(c,d,l,n,r)],1)) $ [ (c,d,l,n,r) | c <- [1,2,3], d <- [1, 3, 4], l <- [1,3], n <- [4], r <- [1,2], (l < d || l == 1) ]
 
-traces = rmdups $ map trans $ replicateM length_trace ['a', 'b', 'c', 'd']
-  where
-    trans tra = map (\x -> if (x == nubbed !! 0) then 'a' else if (x == nubbed !! 1) then 'b' else if (x == nubbed !!2) then 'c' else 'd') tra
-      where nubbed = nub tra
--- rmdups $ map (first -> a...) $ replicateM 4 ['a', 'b']
-rmdups = map head . group . sort
+traces = ["aabbcdbdca", "abccddcaab", "abbccdcdba", "abbccdbcda", "abccddcbba", "aabccdcadb", "aabccddcab", "abccddaacb", "aabccddbca", "aabccdadcb", "abbccddbca", "aabccdcdab", "abbcddbaac", "abbccddacb", "aabbccdacb", "abbcddbcca", "abbccdbdac", "aabbccdcba", "abccddacab", "abbccddbac", "abbccdcbda", "aabbcdbdac", "abccddbbca", "abccddbbac", "aabccdacdb", "aabbcddbca", "abbcdbdaac", "aabbccdbca", "aabccdadbc", "aabbcdbadc", "aabccdcdba", "aabbccdbac", "aabbccdcab", "abbccddcab", "aabbcddbac", "abccdcdbba", "abbccdcdab", "abbcdbdcca", "abccddbcba", "aabccddacb", "abbccddcba", "aabccddcba", "aabbcddcba", "abbccdbdca", "abccdcdaab", "aabccddabc"]
+
+-- -- All patterns removing equivalences (aaabb = bbbaa == cccdd)
+-- traces = rmdups $ map trans $ replicateM length_trace ['a', 'b', 'c', 'd']
+--   where
+--     trans tra = map (\x -> if (x == nubbed !! 0) then 'a' else if (x == nubbed !! 1) then 'b' else if (x == nubbed !!2) then 'c' else 'd') tra
+--       where nubbed = nub tra
+-- rmdups = map head . group . sort
 
 
 test_of_tests_pattern policy hh m assoc = do
@@ -45,12 +51,13 @@ test_all_trace policy hh m assoc trace = do
   r <- mapM (\x -> test_trace file_name policy hh m x trace) initial_states
   let (h,m,ev) = unzip3 r
   let total = length $ filter (== assoc) ev
-  when (total == 256) $ do
-    writeFile (path ++ file_name) ""
-    let pr = (show total) ++ " " ++ (show $ if (m == []) then 0 else maximum m) ++ "\n\ \"
---   let pr = "\n\ \Totally evicted " ++ (show $ length $ filter (== assoc) ev) ++ " out of " ++ (show $ length r) ++ ", max misses = " ++ (show $ maximum m) ++ "\n\ \"
+  let ma = if (m == []) then 0 else maximum m
+  let pr2 = "\n\ \Totally evicted " ++ (show $ length $ filter (== assoc) ev) ++ " out of " ++ (show $ length r) ++ ", max misses = " ++ (show $ maximum m) ++ "\n\ \"
+  when (total == (length $ initial_states)) $ do
+    when save $ writeFile (path ++ file_name) ""
+    let pr = (show total) ++ " " ++ (show ma) ++ "\n\ \"
     when save $ printToFileLn file_name pr
-    
+  when prints $ putStrLn pr2
 
 test_all_pattern policy hh m assoc trace = do
   let file_name = cr_name trace
@@ -74,13 +81,13 @@ test_trace fn policy hh m ll t = do
       _ -> do
         let la = map (\x -> (x, '?')) ll
         return $ (CacheSetContent $ la, length ll)
-  when prints $ putStrLn $ "\n\ \Trace: " ++ (show t) ++  " \n\ \ "
+  when printsAll $ putStrLn $ "\n\ \Trace: " ++ (show t) ++  " \n\ \ "
   -- when save $ printToFileLn $ "\n\ \Trace: " ++ (show trace) ++  " \n\ \ "
-  when prints $ putStrLn $ "Initial Cache Set: " ++ (show l) ++ " \n\ \ "
+  when printsAll $ putStrLn $ "Initial Cache Set: " ++ (show l) ++ " \n\ \ "
   -- when save $ printToFileLn $ "\n\ \Initial Cache Set: " ++ (show l) ++ " \n\ \ " 
   (CacheSetContent csc, Hit h, Hit m) <- policy (if (hh == "hp") then True else False) l (Trace t) m
   let evicted = as - (length $ filter (\(a,b) -> b == '?') csc)
-  when prints $ putStrLn $ (show h) ++ " hits, " ++ (show m) ++ " misses, " ++ (show evicted) ++ " evicted  \n\ \ "
+  when printsAll $ putStrLn $ (show h) ++ " hits, " ++ (show m) ++ " misses, " ++ (show evicted) ++ " evicted  \n\ \ "
   -- when save $ printToFileLn fn $ (show h) ++ " " ++ (show m) ++ " " ++ (show evicted) ++ "\n\ \ "
 --   when save $ printToFileLn fn $ (show h) ++ " hits, " ++ (show m) ++ " misses, " ++ (show evicted) ++ " evicted  \n\ \ "
   return (h,m,evicted)
@@ -131,9 +138,6 @@ brrip :: Bool -> CacheSetContent -> Trace -> Int -> IO(CacheSetContent, HitNumbe
 brrip hp set trace m = do
   r <- rrip True hp set trace m
   return r
-          
-bip_probability = 2
-
 
 rrip :: Bool -> Bool -> CacheSetContent -> Trace -> Int -> IO(CacheSetContent, HitNumber, HitNumber)
 rrip isbrrip hp set trace mbrrip = do
@@ -147,7 +151,7 @@ rrip isbrrip hp set trace mbrrip = do
           Just elem -> do
             let (reg, el) = set !! elem
             let new_cache_st = take elem set ++ [((if hp then 0 else (if reg > 0 then reg-1 else 0)), el)] ++ drop (elem + 1) set
-            if prints then putStrLn $ (show h) ++ " -> Hit: " ++ (show new_cache_st) ++ " \n\ \ " else return()
+            when printsAll $ putStrLn $ (show h) ++ " -> Hit: " ++ (show new_cache_st) ++ " \n\ \ "
             rrip'(Trace (tail trace), CacheSetContent(new_cache_st), Hit $ hit + 1, Hit misses) mbrrip hp isbrrip
           Nothing ->
             case (elemIndex (2^mbrrip-1) $ map (\(a,b) -> a) set) of
@@ -158,17 +162,165 @@ rrip isbrrip hp set trace mbrrip = do
                 let new_cache_st = if b
                       then (take elem set ++ [(2^mbrrip-2, h)] ++ drop (elem + 1) set)
                       else (take elem set ++ [(2^mbrrip-1, h)] ++ drop (elem + 1) set)
-                if prints then putStrLn $ (show h) ++" -> Miss, introduce: " ++ (show new_cache_st) ++ " \n\ \ " else return()
+                when printsAll $ putStrLn $ (show h) ++" -> Miss, introduce: " ++ (show new_cache_st) ++ " \n\ \ "
                 rrip'(Trace $ tail trace, CacheSetContent new_cache_st, Hit hit, Hit $ misses + 1) mbrrip hp isbrrip
               Nothing -> do
                 let new_cache_st = map (\(a,b) -> (a+1,b)) set
-                if prints then putStrLn $ "Miss, increase: " ++ (show new_cache_st) ++ " \n\ \ " else return()
+                when printsAll $ putStrLn $ "Miss, increase: " ++ (show new_cache_st) ++ " \n\ \ "
                 rrip'(Trace trace, CacheSetContent new_cache_st, Hit hit, Hit misses) mbrrip hp isbrrip
         where h = head trace
+
+-- First in first out
+fifo :: Bool -> CacheSetContent -> Trace -> Int -> IO(CacheSetContent, HitNumber, HitNumber)
+fifo _ set trace _ = do
+  (t, s, h, m) <- fifo'(trace, set, Hit 0, Hit 0)
+  return (s, h, m)
+    where
+      fifo' :: (Trace, CacheSetContent, HitNumber, HitNumber) -> IO(Trace, CacheSetContent, HitNumber, HitNumber)
+      fifo' i@(Trace [], _, _, _) = do return i
+      fifo' (Trace trace, CacheSetContent set, Hit hit, Hit misses) =
+        case (elemIndex h $ map (\(a,b) -> b) set) of
+          Just _ -> do
+            when printsAll $ putStrLn $ (show h) ++ " -> Hit: " ++ (show set) ++ " \n\ \ "
+            fifo'(Trace $ tail trace, CacheSetContent set, Hit $ hit + 1, Hit misses)
+          Nothing -> do
+            let new_cache_st = (0,h) : init set
+            when printsAll $ putStrLn $ (show h) ++" -> Miss, introduce: " ++ (show new_cache_st) ++ " \n\ \ "
+            fifo'(Trace $ tail trace, CacheSetContent new_cache_st, Hit hit, Hit $ misses + 1)
+        where h = head trace
+
+
+ -- Least recently used
+lru :: Bool -> CacheSetContent -> Trace -> Int -> IO(CacheSetContent, HitNumber, HitNumber)
+lru b set trace m = do
+  r <- ru False set trace
+  return r
+  
+-- Bimodal insertion policy
+bip :: Bool -> CacheSetContent -> Trace -> Int -> IO(CacheSetContent, HitNumber, HitNumber)
+bip b set trace bip_probability = do
+  r <- ru True set trace
+  return r
+
+-- Bimodal insertion policy
+ru :: Bool -> CacheSetContent -> Trace -> IO(CacheSetContent, HitNumber, HitNumber)
+ru isbip set trace = do
+  (t, s, h, m) <- ru'(trace, set, Hit 0, Hit 0) isbip
+  return (s, h, m)
+    where
+      ru' :: (Trace, CacheSetContent, HitNumber, HitNumber) -> Bool -> IO(Trace, CacheSetContent, HitNumber, HitNumber)
+      ru' i@(Trace [], _, _, _) _ = do return i
+      ru' (Trace trace, CacheSetContent set, Hit hit, Hit misses) isbip = 
+        case (elemIndex h $ map (\(a,b) -> b) set) of
+          Just elem -> do
+            let (reg,_) = set !! elem
+            let new_cache_st = if (elem == ((length set) - 1)) then ((reg,h) : (init set)) else set
+            r <- ru' (Trace (tail trace), CacheSetContent new_cache_st, Hit $ hit + 1, Hit misses) isbip
+            when printsAll $ putStrLn $ (show h) ++ " -> Hit: " ++ (show new_cache_st) ++ " \n\ \ "
+            return r
+          Nothing -> do
+            c <- chance64 bip_probability
+            let b = if isbip then c else True
+            let new_cache_st = if b
+                  then ((fst $ head set,h) : init set)
+                  else (init set ++ [(fst $ last set,h)])
+            when printsAll $ putStrLn $ (show h) ++" -> Miss, introduce: " ++ (show new_cache_st) ++ " \n\ \ "
+            ru'(Trace $ tail trace, CacheSetContent new_cache_st, Hit hit, Hit $ misses + 1) isbip
+        where h = head trace
+
+
+ -- Least recently used
+plru :: Bool -> CacheSetContent -> Trace -> Int -> IO(CacheSetContent, HitNumber, HitNumber)
+plru _ set trace _ = do
+  (t, s, h, m) <- plru'(trace, set, Hit 0, Hit 0)
+  return (s, h, m)
+    where
+      plru' :: (Trace, CacheSetContent, HitNumber, HitNumber) -> IO(Trace, CacheSetContent, HitNumber, HitNumber)
+      plru' i@(Trace [], _, _, _) = do return i
+      plru' (Trace trace, CacheSetContent set, Hit hit, Hit misses) =
+        case (elemIndex h $ map (\(a,b) -> b) set) of
+          Just elem -> do
+            let assoc = length set
+            new_set <- plru_find elem set assoc
+            when printsAll $ putStrLn $ (show h) ++ " -> Hit: " ++ (show new_set) ++ " \n\ \ "
+            plru'(Trace $ tail trace, CacheSetContent new_set, Hit $ hit + 1, Hit misses)
+          Nothing -> do
+            let assoc = length set
+            (up_set, elem) <- plru_insert set assoc
+            let (reg,_) = up_set !! elem
+            let new_set = take elem up_set ++ [(reg, h)] ++ drop (elem + 1) up_set
+            when printsAll $ putStrLn $ (show h) ++" -> Miss, introduce: " ++ (show new_set) ++ " \n\ \ "
+            plru'(Trace $ tail trace, CacheSetContent new_set, Hit hit, Hit $ misses + 1)
+        where h = head trace
+
+        
+plru_insert :: [(Int, Char)] -> Int -> IO(([(Int, Char)], Int))
+plru_insert set associativity = plru_insert' (associativity `div` 2) set ""
+  where
+    plru_insert' :: Int -> [(Int, Char)] -> String -> IO(([(Int, Char)], Int))
+    plru_insert' 0 set list = do
+      let r = readBin list
+      return (set, r)
+    plru_insert' tier set l = do
+      b <- randomRIO(0, 1)
+      let bin = if (a == 2) then b else a
+      if (bin == 0)
+        then do
+        (ret, r) <- plru_insert' (if (tier == 1) then 0 else tier `div` 2) (drop 1 set) (l++(show 0))
+        return (((if (a == 2) then a else 1), val) : ret, r)
+        else do
+        (ret, r) <- plru_insert' (if (tier == 1) then 0 else tier `div` 2) (drop tier set) (l++(show 1))
+        return (((if (a == 2) then a else 0), val):(take (tier - 1) $ tail set) ++ ret, r)
+          where
+            (a, val) = head set
+
+
+plru_find :: Int -> [(Int, Char)] -> Int -> IO([(Int, Char)])
+plru_find address set associativity = plru_find' (associativity `div` 2) set (to_bin address associativity)
+  where
+    plru_find' :: Int -> [(Int, Char)] -> [Int] -> IO([(Int, Char)])
+    plru_find' 0 set list = do return set
+    plru_find' tier set (l:ls) = do
+      if (l == 0)
+        then do
+        ret <- plru_find' (if (tier == 1) then 0 else tier `div` 2) (drop 1 set) ls
+        return (((if (a == 2) then a else 1), val) : ret)
+        else do
+        ret <- plru_find' (if (tier == 1) then 0 else tier `div` 2) (drop tier set) ls
+        return (((if (a == 2) then a else 0), val):(take (tier - 1) $ tail set) ++ ret)
+          where
+            (a, val) = head set
+
+
+to_bin :: Int -> Int -> [Int]
+to_bin n associativity = to_length_bin $ to_bin' n
+  where
+    to_bin' :: Int -> [Int]
+    to_bin' 0 = []
+    to_bin' n | n `mod` 2 == 1 = to_bin' (n `div` 2) ++ [1]
+              | n `mod` 2 == 0 = to_bin' (n `div` 2) ++ [0]
+    to_length_bin :: [Int] -> [Int]
+    to_length_bin l = if ((length l) == (truncate $ logBase (fromIntegral 2) (fromIntegral associativity))) then l else to_length_bin (0:l)
+
+    
+readBin :: String -> Int
+readBin = fromMaybe 0 . toBin
+  where toBin = fmap fst . listToMaybe . readInt 2 (`elem` "01") digitToInt
+
+
+  
+-- Adaptive replacement policy
+-- type AdaptiveRepPol = SetAddresses -> IO(CacheSetContent, HitNumber)
 
 initialSet :: Int -> Int -> IO([(Int, Char)])
 initialSet m associativity = do
   l <- replicateM associativity $ randomRIO(0, 2^m-1)
+  let r = map (\x -> (x, '?')) l
+  return r
+
+initialSetPLRU :: Int -> IO([(Int, Char)])
+initialSetPLRU associativity = do
+  l <- replicateM associativity $ randomRIO(0, 1)
   let r = map (\x -> (x, '?')) l
   return r
 
